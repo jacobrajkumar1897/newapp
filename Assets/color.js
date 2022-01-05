4a3162c4e35db40211aa7966c4a85cd91beed4e3
Ramukjar <raj@doodleblue.com>
2022-01-05 15:40:32 +0530

    new update

---
+++ Assets/color.js
@@ -0,0 +1,18 @@
+
+
+let React = require('react-native');
+
+let {
+  StyleSheet,
+} = React;
+
+let INIT_COLOR = "#fff";
+let INIT_FONT_SIZE = 16; 
+
+
+module.exports = StyleSheet.create({
+  container: {
+      backgroundColor: INIT_COLOR,
+      fontSize: INIT_FONT_SIZE
+  },
+}); 
